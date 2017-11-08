8b2965c3d9b1477a5c162bac5464d7414fa1e397
Glitch (garnet-moose) <none>
2017-11-08 00:39:40 +0000

    🚲📻 trying to simplify

--- sketch.js
+++ sketch.js
@@ -1,18 +1,18 @@
 var serial;          // variable to hold an instance of the serialport library
 var portName = '/dev/cu.usbmodem1421';  // fill in your serial port name here
 var inData;                             // for incoming serial data 
-
 //Moving sprites
-var collage;
-var direction = 90; //circle initial direction moving down
+var body1;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
+  
   //create the sprites
-  collage = createSprite(600, 200, 50, 100);
-  collage.addAnimation("assets/collage_001.png", "assets/collage_002.png", "assets/collage_003.png", "assets/collage_004.png");
+  body1 = createSprite(200, 400);
+  body1.addImage("assets/body1.png");
 
   serial = new p5.SerialPort();       // make a new instance of the serialport library
+  serial.on('list', printList);  // set a callback function for the serialport list event
   serial.on('connected', serverConnected); // callback for connecting to the server
   serial.on('open', portOpen);        // callback for the port opening
   serial.on('data', serialEvent);     // callback for when new data arrives
@@ -24,31 +24,8 @@ function setup() {
 }
 
 function draw() {
-  
-var chickenX = map(inData, 0, 255, 0, 800);
-var chickenY = map(inData, 0, 255, 0, 800);
-
   background(255,255,255);  
-  console.log(chickenX);
-  //aside of setting the velocity directly you can move a sprite
-  //by providing a speed and an angle
-  direction += 2;
-  //speed, angle
-  collage.setSpeed(1, direction);
-  
-  //you can rotate the sprite according the directio it is moving
-  //uncomment this
-  //circle.rotateToDirection = true;
-  
-  collage.position.x = chickenX;
-  collage.position.y = chickenY;
-  //since the force keeps incrementing the speed you can 
-  //set a limit to it with maxSpeed
-  collage.maxSpeed = 1;
-  
-  //draw the sprite
-  drawSprites();
-}
+
 
 function serverConnected() {
   console.log('connected to server.');
