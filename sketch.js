a8ef253dadc30493e5d8cf3ee2d00f6d11b8e71e
livefastdynasty <33458864+livefastdynasty@users.noreply.github.com>
2017-11-07 20:57:51 -0500

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -2,18 +2,19 @@ var serial;          // variable to hold an instance of the serialport library
 var portName = '/dev/cu.usbmodem1421';  // fill in your serial port name here
 var inData;                             // for incoming serial data 
 //Moving sprites
-var collage;
-var direction = 90; //circle initial direction moving down
+var body1, body2, body3, gem, eye1;
 
 function setup() {
-  createCanvas(800,400);
-  
+  createCanvas(windowWidth, windowHeight);
   //create the sprites
-  collage = createSprite(600, 200, 50, 100);
-  collage.addAnimation("assets/collage_001.png", "assets/collage_002.png", "assets/collage_003.png", "assets/collage_004.png");
+  gem = createSprite(600, 200);
+  gem.addImage("normal", loadImage("assets/gem.png"));
+  gem.addImage("eye", loadImage("assets/eye01.png"));
+  
+  body1 = createSprite(400, 200);
+  body1.addImage(loadImage("assets/body01.png"))
 
   serial = new p5.SerialPort();       // make a new instance of the serialport library
-  serial.on('list', printList);  // set a callback function for the serialport list event
   serial.on('connected', serverConnected); // callback for connecting to the server
   serial.on('open', portOpen);        // callback for the port opening
   serial.on('data', serialEvent);     // callback for when new data arrives
@@ -25,24 +26,28 @@ function setup() {
 }
 
 function draw() {
-  background(255,255,255);  
   
+var gemX = map(inData, 0, 255, 0, width);
+
+  background(255,255,255);  
+  console.log(gemX);
   //aside of setting the velocity directly you can move a sprite
   //by providing a speed and an angle
-  direction += 2;
-  //speed, angle
-  collage.setSpeed(3, direction);
+  
   
   //you can rotate the sprite according the directio it is moving
   //uncomment this
   //circle.rotateToDirection = true;
   
-  //or by applying a force toward a point
-  //force (acceleration), pointx, pointy
-  collage.attractionPoint(.2, inData, 100);
+  gem.position.x = gemX;
+  gem.position.y = mouseY;
   //since the force keeps incrementing the speed you can 
   //set a limit to it with maxSpeed
-  collage.maxSpeed = 5;
+ 
+  if(gem.overlap(body1))
+    gem.changeAnimation("eye");
+  else
+    gem.changeAnimation("normal");
   
   //draw the sprite
   drawSprites();
